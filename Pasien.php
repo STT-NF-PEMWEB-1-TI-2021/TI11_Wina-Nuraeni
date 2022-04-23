b3bbc48e997741d5b093162f8a27f709951700cd
Wina nuraeni <91929364+wnrnii@users.noreply.github.com>
2022-04-23 22:02:26 +0700

    Add files via upload

---
+++ Pasien.php
@@ -0,0 +1,105 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+<div>
+    <center><p> FORM DATA PASIEN<p><center>
+        <hr>
+<form action="proses.php"  method="POST">
+  <div class="form-group row">
+    <label for="id" class="col-4 col-form-label">Id</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-500px"></i>
+          </div>
+        </div> 
+        <input id="id" name="id" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="kode" class="col-4 col-form-label">Kode</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-adjust"></i>
+          </div>
+        </div> 
+        <input id="kode" name="kode" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nama_pasien" class="col-4 col-form-label">Nama Pasien</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-ambulance"></i>
+          </div>
+        </div> 
+        <input id="nama_pasien" name="nama_pasien" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="tmp_lahir" class="col-4 col-form-label">Tempat Lahir</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-american-sign-language-interpreting"></i>
+          </div>
+        </div> 
+        <input id="tmp_lahir" name="tmp_lahir" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="tgl_lahir" class="col-4 col-form-label">Tanggal Lahir</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-angle-double-left"></i>
+          </div>
+        </div> 
+        <input id="tgl_lahir" name="tgl_lahir" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="email" class="col-4 col-form-label">Email</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text"></div>
+        </div> 
+        <input id="email" name="email" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Gender</label> 
+    <div class="col-8">
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="jk" id="jk_0" type="radio" class="custom-control-input" value="L"> 
+        <label for="jk_0" class="custom-control-label">Laki-Laki</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="jk" id="jk_1" type="radio" class="custom-control-input" value="P"> 
+        <label for="jk_1" class="custom-control-label">Perempuan</label>
+      </div>
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <input type="submit" class="btn btn-primary" value="Daftar" name="proses"/></button>
+    </div>
+  </div>
+</form>
+         
+</div>
